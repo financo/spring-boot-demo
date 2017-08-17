b27e9f6a4d77e3b6e3936c875705bcf3184a670e
wangzhengyu <wangzhengyu@bonc.com.cn>
2017-08-17 15:08:02 +0800

    添加事务

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -3,12 +3,14 @@ package com;
 import org.springframework.boot.SpringApplication;  
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.bonc.repository.BaseRepositoryFactoryBean;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableJpaRepositories(repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
+@EnableTransactionManagement
 @SpringBootApplication
 @EnableSwagger2
 public class Application {  
